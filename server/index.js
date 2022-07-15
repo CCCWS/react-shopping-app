7fd5a691f3ec85dde7214e66e5a732caaac01ac8
CCCWS <eung0626@gmail.com>
2022-07-15 22:37:18 +0900

    shopping

--- server/index.js
+++ server/index.js
@@ -29,7 +29,7 @@ if (process.env.NODE_ENV === "production") {
   });
 }
 
-app.listen(process.env.PORT || 3001, function () {
+app.listen(process.env.PORT || 3000, function () {
   console.log(
     "Express server listening on port %d in %s mode",
     this.address().port,
