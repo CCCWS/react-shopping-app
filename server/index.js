6c654961008b0c06c7929e94677d5b755d206493
CCCWS <eung0626@gmail.com>
2022-07-16 00:36:22 +0900

    shopping

--- server/index.js
+++ server/index.js
@@ -30,7 +30,7 @@ app.use("/api/product", require("./models/product")); //해당 경로로 이동�
 
 if (process.env.NODE_ENV === "production") {
   // Set static folder
-  app.use(express.static("client/build"));
+  app.use(express.static("./client/build"));
 
   app.get("*", (request, response) => {
     response.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
