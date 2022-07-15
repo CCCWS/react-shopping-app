550b6b686551a607e9d2fb246c041dfcda3ce331
CCCWS <eung0626@gmail.com>
2022-07-15 23:08:41 +0900

    shopping

--- server/index.js
+++ server/index.js
@@ -29,13 +29,13 @@ if (process.env.NODE_ENV === "production") {
   });
 }
 
-app.listen(process.env.PORT || 3001, function () {
-  console.log(
-    "Express server listening on port %d in %s mode",
-    this.address().port,
-    app.settings.env
-  );
-});
+// app.listen(process.env.PORT || 3001, function () {
+//   console.log(
+//     "Express server listening on port %d in %s mode",
+//     this.address().port,
+//     app.settings.env
+//   );
+// });
 
 mongoose
   .connect(config.mongoURI)
