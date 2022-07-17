cce7789aeeb20108e68c17437e66e39223f5b8dc
CCCWS <eung0626@gmail.com>
2022-07-18 03:36:16 +0900

    shopping

--- client/src/setUpProxy.js
+++ client/src/setUpProxy.js
@@ -4,7 +4,7 @@ const { createProxyMiddleware } = require("http-proxy-middleware");
 module.exports = function (app) {
   app.use(
     createProxyMiddleware("/api", {
-      target: "http://localhost:5000",
+      target: "http://localhost:3001",
       changeOrigin: true,
     })
   );
