5ab9bb857c010967c43abc68bbe3805f4ae59d83
CCCWS <eung0626@gmail.com>
2022-07-18 02:25:09 +0900

    shopping

--- server/index.js
+++ server/index.js
@@ -32,9 +32,9 @@ app.use("/uploads", express.static("uploads")); //nodejs에서 정적파일을 �
 app.use("/api/product", require("./models/product")); //해당 경로로 이동하여 처리
 
 if (process.env.NODE_ENV === "production") {
-  // app.use(express.static("client/build"));
+  app.use(express.static("client/build"));
   app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "../client", "build", "index.html"));
+    res.sendFile(path.resolve(__dirname, "../client", "Page", "Main.js"));
   });
 }
 
