adb87ee8a3675932ef66a3bd3063fc51b9c6387c
CCCWS <eung0626@gmail.com>
2022-07-20 21:37:48 +0900

    shopping

--- client/src/Components/ProductRank.js
+++ client/src/Components/ProductRank.js
@@ -1,5 +1,8 @@
 import React, { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
 import axios from "axios";
+import { Carousel } from "antd";
+import { postUrl } from "../PostUrl";
 import "./ProductRank.css";
 
 function ProductRank() {
@@ -21,11 +24,55 @@ function ProductRank() {
       console.log(err, "조회실패");
     }
   };
-
-  console.log(sold);
   console.log(views);
 
-  return <div className="ProductRank">ProductRank</div>;
+  return (
+    <div className="ProductRank">
+      <Carousel effect="fade">
+        <div className="ProductRank-section">
+          <div>
+            <strong>조회수</strong>가 높은 상품
+          </div>
+          <ProductSection items={views} />
+        </div>
+
+        <div className="ProductRank-section">
+          <div>
+            <strong>판매량</strong>이 높은 상품
+          </div>
+          <ProductSection items={sold} />
+        </div>
+      </Carousel>
+    </div>
+  );
 }
+
+const ProductSection = ({ items }) => {
+  const nav = useNavigate();
+  return (
+    <div className="ProductRank-product">
+      {items.map((data, index) => (
+        <div
+          className="ProductRank-product-item"
+          key={index}
+          onClick={() => nav(`/product/${data._id}`)}
+        >
+          <div
+            className="ProductRank-product-item-img"
+            style={{
+              backgroundImage: `url('${postUrl}${data.image[0].name}')`,
+            }}
+          >
+            <div className="ProductRank-product-item-number">
+              {items.indexOf(data) + 1}
+            </div>
+          </div>
+
+          <div className="ProductRank-product-item-title">{data.title}</div>
+        </div>
+      ))}
+    </div>
+  );
+};
 
 export default ProductRank;
