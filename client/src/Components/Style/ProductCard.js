accabb5b5e4b5d5813c88fb856df2a833b37c3b0
CCCWS <eung0626@gmail.com>
2022-10-22 03:35:02 +0900

    shopping

---
+++ client/src/Components/Style/ProductCard.js
@@ -0,0 +1,59 @@
+import styled from "styled-components";
+
+export const Card = styled.div`
+  min-height: 10rem;
+  width: 100%;
+  padding: 1rem;
+  border-radius: 5px;
+  margin-top: 1rem;
+  margin-bottom: 1.5rem;
+  display: flex;
+  justify-content: space-between;
+`;
+
+export const Image = styled.div`
+  background-image: ${(props) => props.img};
+  background-size: cover;
+  background-position: center;
+  background-repeat: no-repeat; ;
+`;
+
+export const Title = styled.div`
+  font-weight: 600;
+  font-size: 1.1rem;
+  white-space: normal;
+`;
+
+export const Price = styled.div`
+  color: var(--red);
+  font-size: 1rem;
+`;
+
+export const Time = styled.div`
+  font-size: 1rem;
+  color: var(--gray);
+`;
+
+export const Count = styled.div`
+  font-size: 0.8rem;
+  font-weight: 500;
+  color: var(--gray);
+`;
+
+// export const CountAndPrice = styled.div`
+//   display: flex;
+//   justify-content: center;
+//   align-items: center;
+//   flex-direction: column;
+//   align-items: flex-end;
+
+//   min-width: 5rem;
+//   font-size: 1rem;
+// `;
+
+// export const List = styled.div`
+//   display: flex;
+//   flex-wrap: wrap;
+//   width: 100%;
+//   margin: auto;
+// `;
