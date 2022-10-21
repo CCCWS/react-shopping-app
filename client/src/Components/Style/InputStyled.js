ebbb157c084a6846ac9326e701d4ecdf2c7ae793
CWS <86645532+CCCWS@users.noreply.github.com>
2022-10-22 03:20:46 +0900

    Create InputStyled.js

---
+++ client/src/Components/Style/InputStyled.js
@@ -0,0 +1,73 @@
+import styled, { css } from "styled-components";
+
+export const Section = styled.div`
+  display: flex;
+  align-items: center;
+  position: relative;
+  width: 100%;
+  margin: auto;
+  padding: 1.5rem;
+  font-size: 1rem;
+
+  & > :first-child {
+    min-width: 8rem;
+    padding: 0.5rem;
+    font-weight: 700;
+
+    @media (max-width: 550px) {
+      min-width: 100px;
+    }
+  }
+`;
+
+export const Input = styled.input`
+  border: 2px solid transparent;
+  background-color: ${(props) =>
+    props.darkMode ? "var(--black)" : "var(--gray_transparency)"};
+
+  width: ${(props) =>
+    //UploadForm Components
+    (props.inputType === "title" && "70%") ||
+    (props.inputType === "price" && "200px") ||
+    (props.inputType === "count" && "100px") ||
+    //CheckOut Page
+    (props.inputType === "name" && "200px") ||
+    (props.inputType === "phone" && "200px") ||
+    (props.inputType === "address" && "100%") ||
+    (props.inputType === "req" && "100%")};
+  height: 3rem;
+  /* margin-right: 10px; */
+  padding: 0.5rem;
+  border-radius: 5px;
+  outline: none;
+
+  &:focus {
+    border: 2px solid var(--red_transparency);
+  }
+
+  ${(props) =>
+    (props.inputType === "price" || props.inputType === "count") &&
+    css`
+      &::-webkit-outer-spin-button,
+      &::-webkit-inner-spin-button {
+        -webkit-appearance: none;
+      }
+    `}
+`;
+
+export const Textarea = styled.textarea`
+  border: 2px solid transparent;
+  background-color: ${(props) =>
+    props.darkMode ? "var(--black)" : "var(--gray_transparency)"};
+  border-radius: 5px;
+  width: 100%;
+  height: 10rem;
+  padding: 0.5rem;
+  outline: none;
+  resize: none;
+  overflow-y: scroll;
+
+  &:focus {
+    border: 2px solid var(--red_transparency);
+  }
+`;
