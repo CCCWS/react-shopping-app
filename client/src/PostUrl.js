25c48abb3cc5abae16df1accfde351d2d1740ff7
CCCWS <eung0626@gmail.com>
2022-12-21 18:05:41 +0900

    shopping

--- client/src/PostUrl.js
+++ client/src/PostUrl.js
@@ -2,3 +2,5 @@ export const postUrl =
   process.env.NODE_ENV === "production"
     ? "http://3.39.126.49:3001/uploads/"
     : "http://localhost:3001/uploads/";
+
+console.log(process.env.NODE_ENV);
