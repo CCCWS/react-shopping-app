306516743004dcbebb337d5482951510cbd3b1cc
CCCWS <eung0626@gmail.com>
2023-01-30 21:33:44 +0900

    shopping

--- client/src/Page/test/Select2.js
+++ client/src/Page/test/Select2.js
@@ -1,12 +1,33 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import styled from "styled-components";
+import axios from "axios";
+
+const API_KEY = "17058e11301a4bf892f409d4ea85d5ff";
+const SEARCH_SIZE = 5;
 
 const Select2 = () => {
   const [value, setValue] = useState("");
+  const [searchResult, setSearchResult] = useState([]);
+
+  useEffect(() => {
+    const getApi = async () => {
+      const res = await axios.get(
+        `https://api.rawg.io/api/games?key=${API_KEY}&search=${value}&page_size=${SEARCH_SIZE}`
+      );
+      console.log(res.data.results);
+      setSearchResult(res.data.results);
+    };
+
+    if (value) getApi();
+  }, [value]);
   return (
     <Div>
       <Input onChange={(e) => setValue(e.target.value)} />
-      <ItemBox value={value} />
+      <ItemBox value={value}>
+        {searchResult.map((data, index) => (
+          <React.Fragment key={index}>{data.name}</React.Fragment>
+        ))}
+      </ItemBox>
     </Div>
   );
 };
