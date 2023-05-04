36d0140c55d5407229d22e79c19bf00cbae41cec
CCCWS <eung0626@gmail.com>
2023-05-05 03:02:52 +0900

    shopping

--- client/src/Page/Test.js
+++ client/src/Page/Test.js
@@ -7,9 +7,12 @@ const Test = () => {
       // const res = await axios.post("/api/product/productDetail", {
       //   id: "62bc572a1973d6509eb5f89b",
       // });
-      const res = await axios.get(
-        "https://jsonplaceholder.typicode.com/todos/1"
-      );
+      // const res = await axios.get(
+      //   "https://jsonplaceholder.typicode.com/todos/1"
+      // );
+
+      const res = await axios.get("http://3.36.235.60:3000");
+
       console.log(res.data);
     };
 
