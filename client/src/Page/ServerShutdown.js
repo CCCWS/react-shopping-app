999b0f63406e9987a217401de19a5b31755004c9
CCCWS <eung0626@gmail.com>
2023-05-12 01:37:47 +0900

    shopping

--- client/src/Page/ServerShutdown.js
+++ client/src/Page/ServerShutdown.js
@@ -1,10 +1,14 @@
 import React, { useEffect } from "react";
 import styled from "styled-components";
+import closeGif from "../data/serverCloseGif.gif";
 
 const ServerShutdown = () => {
   return (
     <Page>
       <h2>지금은 서버가 닫혀있어요.</h2>
+      <Time>KST AM 08:00 ~ PM 10:00</Time>
+
+      <CloseGif src={closeGif}></CloseGif>
     </Page>
   );
 };
@@ -12,11 +16,30 @@ const ServerShutdown = () => {
 const Page = styled.div`
   width: 100vw;
   height: 100vh;
-  /* background-color: gray; */
+
+  background-color: rgba(200, 200, 200);
 
   display: flex;
+  flex-direction: column;
   justify-content: center;
   align-items: center;
+`;
+
+const Time = styled.div`
+  font-size: 1.2rem;
+  color: red;
+`;
+
+const CloseGif = styled.img`
+  width: auto;
+  height: 300px;
+
+  position: absolute;
+  bottom: 10px;
+  right: 0px;
+
+  /* background-color: red; */
+  object-fit: cover;
 `;
 
 export default ServerShutdown;
